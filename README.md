297993c84f9b9b315983e58360029e26af0fac6d
ebisLab <ebaiden@techie.com>
2019-06-13 17:57:13 -0400

    instructor-class

--- README.md
+++ README.md
@@ -37,6 +37,7 @@ const fred = new Person({
 });
 ```
 
+
 * Because none of the above code is new, you're about to see your world get much much easier when dealing with Object Creation and Classical Inheritance as it pertains to JavaScript.
 * The Class Keyword makes this SO MUCH EASIER!
 * **Fork** and clone this repository.
--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,1 +1,47 @@
 // CODE here for your Lambda Classes
+
+class Person{ //base-class
+    constructor(persattr) {
+
+    this.name = persattr.name;
+    this.location = persattr.location;
+    this.age= persattr.age;
+  }
+  
+  speak(){ //function
+      return `Hello my name is ${this.name}, I am from ${this.location}`
+    }
+}
+
+
+
+class Instructors extends { //base-class
+    constructor(instattr) {
+        super(instattr); //the binding part
+
+    this.specialty = instattr.specialty;
+    this.favLanguage = instattr.favLanguage;
+    this.catchPhrase = instattr.catchPhrase;
+  }
+  
+  demo(subject){ //function
+      console.log(`Today we are learning about 
+      ${subject} where subject is the param passed in.`)
+    }
+
+   grade(student, subject){ //function
+        console.log(`${this.name} receives a perfect score on ${this.subject}.`)
+      }
+}
+
+
+
+
+// const fred = new Instructor({
+//     name: 'Fred',
+//     location: 'Bedrock',
+//     age: 37,
+//     favLanguage: 'JavaScript',
+//     specialty: 'Front-end',
+//     catchPhrase: `Don't forget the homies`
+//   });
